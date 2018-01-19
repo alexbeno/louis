cbb1a4d215698af57a85da10352cb19c65e375b3
alexbeno <alexbenoliel@gmail.com>
2018-01-19 08:41:21 +0100

    ✨ add galerie page

---
+++ wp-content/themes/flinked/page-template/page-galerie.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Galerie
+ *
+ * @package WordPress
+ * @subpackage Flinked
+ */
+
+$folder = 'galerie';
+$path = 'template/'. $folder .'/'.$folder.'-';
+
+if ( have_posts() ){
+  while ( have_posts() ) {
+    the_post();
+    get_header();
+    get_template_part($path . 'main');
+    get_footer();
+  }
+}
+?>
