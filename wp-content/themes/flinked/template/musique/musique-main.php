f1404cd9a68ba719449537cd62a2f4149917504f
alexbeno <alexbenoliel@gmail.com>
2018-01-21 21:51:31 +0100

    🚀 get acf content

--- wp-content/themes/flinked/template/musique/musique-main.php
+++ wp-content/themes/flinked/template/musique/musique-main.php
@@ -1,16 +1,33 @@
 
 <?php
-$name = get_field( "home--nom" );
-$musicNav = get_field('option--musique', 'option');
+//option page
 $galerieNav = get_field('option--galerie', 'option');
-$dragNav = get_field('option--drag', 'option');
 $scrollNav = get_field('option--scroll', 'option');
 $galeriePage = get_field('option--galerie_page', 'option');
+
+//album content global
+$title = get_field( "album--nom" );
+$description = get_field( "album--description" );
+$pochet = get_field( "album--pochette" );
+$musique = get_field( "album--musique" );
+
+//network link
+$appleMusique = get_field( "album--apple_musique" );
+$spotify = get_field( "album--spotify" );
+$deezer = get_field( "album--deezer" );
+
 ?>
 
 <main>
   <div class="musiquePage">
+<?php
+$query = new WP_Query( array( 'post_type' => 'album' ) );
 
+if ( $query->have_posts() ) : ?>
+  <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+  
+  <?php endwhile; endif; wp_reset_postdata(); ?>
 
   </div>
 </main>
