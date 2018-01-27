84c47f1a39bdf4b0c861f96cf61033bb072b5059
alexbeno <alexbenoliel@gmail.com>
2018-01-28 00:28:44 +0100

    🔥 remove include drag script

--- wp-content/themes/flinked/inc/core/_enqueu.php
+++ wp-content/themes/flinked/inc/core/_enqueu.php
@@ -18,13 +18,11 @@ add_action( 'wp_enqueue_scripts',  'my_style' );
  */
 function my_script() {
     wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' , array(), true);
-    wp_register_script( 'drag', 'https://cdnjs.cloudflare.com/ajax/libs/interact.js/1.2.8/interact.min.js' , array(), true);
     wp_register_script( 'lethargy', LIB_URL . '/lethargy.js' , array(), true);
     wp_register_script( 'main', JS_URL . '/bundle.js' , array(), true);
     wp_localize_script( 'main', 'ajaxurl', admin_url( 'admin-ajax.php' ) );
 
     wp_enqueue_script('jquery');
-    wp_enqueue_script('drag');
     wp_enqueue_script('lethargy');
     wp_enqueue_script('main');
 }
